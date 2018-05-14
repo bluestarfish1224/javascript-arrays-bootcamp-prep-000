62e78d9ff9830886009ef2e1741301fad1af3d04
loretta_okoye@hotmail.com <loretta_okoye@hotmail.com>
2018-05-14 04:45:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 function addElementToBeginningOfArray(array,element){
-  newarray = [element, ]
+  var newArray = [element,]
 }
 function destructivelyAddElementToBeginningOfArray
